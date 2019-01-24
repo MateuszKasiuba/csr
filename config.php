b3e377e852db68fdd52b86e6c2784ed299d97167
Monika Sadlok <mon.sadlok@gmail.com>
2019-01-24 15:02:58 +0100

    csr

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+	$title = 'Omarsys CSR Generator';
+
+	$bootstrap_cdn = '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap';
+	$jquery_cdn = '//cdnjs.cloudflare.com/ajax/libs/jquery';
+	//$bootstrap_cdn = '//maxcdn.bootstrapcdn.com/bootstrap';
+	//$jquery_cdn = '//ajax.googleapis.com/ajax/libs/jquery';
+
+	//openssl config
+	$openssl_config = array(
+		"digest_alg" => "sha256",
+		"private_key_bits" => 2048,
+		'private_key_type' => OPENSSL_KEYTYPE_RSA
+	);
+
+
+
+
